703616e9b2e18ac8225abb6bebd51a50135f87ec
Aiman2027 <aimannishat78692@gmail.com>
2026-08-23 23:53:49 +0530

    Fix recipe image size and center alignment

--- frontend/app.py
+++ frontend/app.py
@@ -2626,12 +2626,7 @@ if "detected_ingredients" in st.session_state:
                             ).format(err=str(e))
                         )
 
-<<<<<<< HEAD
                 if st.session_state.get(
-=======
-
-             if st.session_state.get(
->>>>>>> fb9a09b8b7ae11eb14d6db9239783b88abb90550
                 "recipe_image"
             ):
 
